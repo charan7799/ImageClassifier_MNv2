367205d484bbb8b70e36893980a57bdd5ab1f212
charan7799 <charupallicharan@gmail.com>
2022-10-14 14:40:25 +0530

    pointed the error

--- backend/src/classification.py
+++ backend/src/classification.py
@@ -2,16 +2,17 @@
 import torch
 from torch.nn import functional as F
 import sys
-
+import os
 
  
 # getting the processing modules by adding the path of the folder to sys
-sys.path.insert(0, r'S:\ds_portfolio_proj\clf_imageNet_mnV2\backend')
-
+# sys.path.insert(0, r'S:\ds_portfolio_proj\clf_imageNet_mnV2\backend')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# print(sys.path)
 # from preprocess import *
 # from get_inferences import * 
 # from get_inferences import *
-from processing.preprocess import preprocess_image, get_inference
+from processing.preprocess import preprocess_image, get_inference   
 
 def get_classification(input_image, inference_file):
     # show_image(image_file)
